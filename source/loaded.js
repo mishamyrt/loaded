a0e2e919ad1d9b373495ab56085ac48c6e634941
mishamyrt <misha@myrt.co>
2018-12-11 09:48:19 +0300

    Added build for amd and browser

---
+++ source/loaded.js
@@ -0,0 +1,9 @@
+const {
+    imagesLoaded,
+    imageLoaded,
+    nodeLoaded,
+} = require('./index.js')
+
+window.imagesLoaded = imagesLoaded
+window.imageLoaded = imageLoaded
+window.nodeLoaded = nodeLoaded
