dd7f28a19530b0a03338275d35d0a24326657758
mishamyrt <misha@myrt.co>
2018-12-14 21:56:35 +0300

    Removed files

--- source/loaded.js
+++
@@ -1,9 +0,0 @@
-const {
-    imagesLoaded,
-    imageLoaded,
-    nodeLoaded,
-} = require('./index.js')
-
-window.imagesLoaded = imagesLoaded
-window.imageLoaded = imageLoaded
-window.nodeLoaded = nodeLoaded
